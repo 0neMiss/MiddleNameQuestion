ab0e23a453d8050f960fa98ee492fc61e6d93f34
0neMiss <jordanshehane@gmail.com>
2021-11-10 15:00:33 -0700

    setting up instructions and removing the solution

--- src/App.js
+++ src/App.js
@@ -1,43 +1,24 @@
-import React, { useState } from "react";
-import NameList from "./NameList";
+import React from "react";
+/*
 
+This question is split into three different parts finish as much as you can with the time provided. It would be preferred if you 
+used hooks to solve this problem. Bonus points if you dont need to use useEffect.
+
+First, take a look at the data.js file, you will see that the App component is being passed an array of objects with some names.
+Iterate through the names and display them inside of a list tag.
+
+Write state handling so that when we click on one of the names in the list it will "focus" that element, you can represent 
+this with a visual if it helps but it isnt neccisary to do so. If you can prove that its being selected thats all that matters, we are going to use
+this functionality on the next part.
+
+Lastly add an input box and a button that allows us to set the middle name value for the selected name.
+
+So if I click on the name "Amy Williams" and then type "Lee" in the input box and click the button, its expected that the name on the list will update to 
+"Amy Lee Williams"
+
+*/
 function App({ data }) {
-  const [names, setNames] = useState(data);
-  const [selectedIndex, setSelectedIndex] = useState(null);
-  const [middleName, setMiddleName] = useState("");
-  const handleChange = (e) => {
-    setMiddleName(e.target.value);
-  };
-  const handleSubmit = () => {
-    console.log(selectedIndex);
-    const newName = { ...names[selectedIndex], middleName: middleName };
-    const newNames = [...names];
-    newNames[selectedIndex] = newName;
-    setNames(newNames);
-    setMiddleName("");
-  };
-  console.log(names);
-
-  return (
-    <div className="App">
-      <NameList {...{ names, setSelectedIndex }} />
-
-      <input
-        style={{ marginLeft: "22px" }}
-        onChange={(e) => {
-          handleChange(e);
-        }}
-      ></input>
-      <button
-        style={{ marginLeft: "20px" }}
-        onClick={(e) => {
-          handleSubmit(e);
-        }}
-      >
-        add a middle name
-      </button>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
